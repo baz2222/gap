4d4cc60b22cdd656f549d450d4a73a1223aa8166
Vasyl Velhus <vasyl.velhus@gmail.com>
2021-03-15 21:34:09 +0200

    Empty Intellij Idea 2020.1 project.

---
+++ desktop/src/com/juniorgames/gap/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.juniorgames.gap.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.juniorgames.gap.GapGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GapGame(), config);
+	}
+}
